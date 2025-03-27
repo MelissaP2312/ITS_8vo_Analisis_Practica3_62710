52b85b3b10fd2e7722c465c2fa8a525d66e566d2
MelissaP2312 <al062710@uacam.mx>
2025-03-27 16:04:19 -0600

    Frontend

---
+++ src/infrastructure/services/AuthServiceImpl.ts
@@ -0,0 +1,20 @@
+import {User} from "../../core/domain/User";
+import {AuthService} from "../../core/ports/AuthService";
+
+export class AuthServiceImpl implements AuthService {
+    async login(email: string, password: string): Promise<User | null> {
+        if(email === "user@example.com" && password === "password"){
+            return {
+                id: "1",
+                email,
+                name: "John Doe",
+                token: "fake-jwt-token"
+            };
+        }
+        return null;
+    }
+
+    async logout(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+}
